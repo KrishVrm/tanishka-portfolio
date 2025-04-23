f7bd345dcd6229214fde0814143ddd4a3bce7b77
Krish <potatoduke2005@gmail.com>
2025-04-23 16:10:42 +0530

    mobile responsive

--- src/globalComponents/Header.jsx
+++ src/globalComponents/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 // import "./_header.css"; // Ensure you create a CSS file for styling
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -10,16 +17,24 @@ const Header = () => {
       <nav className="navbar">
         <ul className="nav-links">
           <li>
-            <a href="#home">Home</a>
+            <a href="#home">
+              <FaFacebook />
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#about">
+              <FaGithub />
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="https://www.linkedin.com/in/tanishka-agrawal-m1r1t1v2/">
+              <FaLinkedin />
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact">
+              <FaTwitter />
+            </a>
           </li>
         </ul>
       </nav>
